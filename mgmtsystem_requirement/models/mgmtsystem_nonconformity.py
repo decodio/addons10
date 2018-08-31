f783d7bb70cdc3b8578fe9b6fe232f98bce724e3
dario <dario.meniss@slobodni-programi.hr>
2018-08-31 16:21:05 +0200

    [mgmtsystem] Override create from OCA (without super) so we can inherit without triggering sequence

--- mgmtsystem_requirement/models/mgmtsystem_nonconformity.py
+++ mgmtsystem_requirement/models/mgmtsystem_nonconformity.py
@@ -18,3 +18,16 @@ class MgmtsystemNonconformity(models.Model):
     @api.onchange('system_id')
     def onchange_system(self):
         self.requirement_id = None
+
+    @api.model
+    def create(self, vals):
+        """Override of create with direct call to models.Model.create()
+           so we can inherit and update vals without triggering sequence before
+           or after calling super
+        """
+        if vals.get('ref', 'NEW') == 'NEW':
+            vals.update({
+                'ref': self.env['ir.sequence'].next_by_code(
+                    'mgmtsystem.nonconformity')
+            })
+        return super(models.Model, self).create(vals)
